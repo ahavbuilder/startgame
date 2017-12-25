dba02efeda9505ec63c1c6532d9d885ec348876d
ahavbuilder <hookoloop@gmail.com>
2017-12-25 21:02:44 +0300

    demo

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php
+    $name = 'Алексей';
+    $age = '29';
+    $email = '<a href="hookoloop@gmail.com">hookoloop@gmail.com</a>';
+    $city = 'Москва';
+    $about = 'Жрец ножа и вилки';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+  <body>
+    <h1>Страница пользователя Алексей</h1>
+
+    <p>Имя: <?=$name ?> </p>
+    <p>Возраст: <?=$age ?> </p>
+    <p>Электронный Адрес: <?=$email ?> </p>
+    <p>Город: <?=$city ?> </p>
+    <p>О себе: <?=$about ?> </p>
+
+  </body>
+</html>
