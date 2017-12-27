1641041d8f404b82a19d9caab083497ffdc02897
ahavbuilder <hookoloop@gmail.com>
2017-12-27 07:56:47 +0300

    second try

--- lesson2.php
+++ lesson2.php
@@ -1,18 +1,28 @@
 <?php
     $x = rand(0,100);
         echo "3"
-        $a = 1;
-        $b = 1;
-    if($a > 3);
-{
-        echo "a > 3"
+          $a = 1;
+          $b = 1;
+    if($a > 3)
+{       echo "a > 3";
 }
-     $a => false
-    elseif ()
-{
-        echo "$a<=5"
-}
+    else($a < 3)
+{       echo "a < 3";
 
+}
+    if = ($a || $x);
+{
+    echo "true or false"
+}
+     if($a == 3)
+     {
+       echo "a = 3"
+     }
+     else($a < 3)
+     {
+       echo "a = 3"
+     }
+     switch 
 
 
 
