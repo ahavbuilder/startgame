c7eeec25a756260dd881ffc3d14987df4ca1ed75
ahavbuilder <hookoloop@gmail.com>
2018-01-10 18:00:05 +0300

    continue

--- lesson2.php
+++ lesson2.php
@@ -1,28 +1,44 @@
 <?php
-    $x = rand(0,100);
-        echo "3"
-          $a = 1;
-          $b = 1;
-    if($a > 3)
-{       echo "a > 3";
-}
-    else($a < 3)
-{       echo "a < 3";
+$x = rand(0,100);
+$a = 1;
+$b = 1;
 
-}
-    if = ($a || $x);
+
+if($a > $x)
+
+    {
+        echo 'задуманное число НЕ входит в числовой ряд';}
+else($a < $x)
 {
-    echo "true or false"
+        echo ($a < $x)
 }
-     if($a == 3)
-     {
-       echo "a = 3"
-     }
-     else($a < 3)
-     {
-       echo "a = 3"
-     }
-     switch 
+
+if($a == $x)
+{
+        echo 'задуманное число входит в числовой ряд';}
+else($a == $x)
+{
+        echo ($a == $x)
+}
+
+$myArr = array(
+         $a
+         $b
+         $x => $a
+)
+              {
+
+
+        }
+while($a < count($myArr);
+        echo $x. "<br/>";
+do{
+        echo $x. "<br/>"
+  $x = $a + $b;
+}while($x = $a + $b);{
+        echo $x;}
+while($x == $b)
+
 
 
 
