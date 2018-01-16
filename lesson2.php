8d02cd9ad3c41381d5795efe437b3533e48aeee1
ahavbuilder <hookoloop@gmail.com>
2018-01-16 10:00:54 +0300

    last version

--- lesson2.php
+++ lesson2.php
@@ -4,17 +4,18 @@ $a = 1;
 $b = 1;
 
 
+while ($a < $x) {
+    echo "$x = $a";
+    echo "$a = $a + $b";
+    echo "$b = $a + $X";
+}
+
+
+
 if ($a > $x)
 
 {
     echo 'задуманное число НЕ входит в числовой ряд';
-}
-// elseif ($a < $x)
-// {
-//   echo "$a < $x";
-// }
-else {
-  echo "$a < $x";
 }
 
 
@@ -22,23 +23,4 @@ if ($a == $x)
 {
     echo 'задуманное число входит в числовой ряд';
   }
-// elseif ($a != $x)
-// {
-//   echo "$a != $x";
-// }
-// else {
-//   echo "$a != $x";
-// }
-while ($a < $x) {
-  echo "$x";
-break;
-}
-switch ($a + $b){
-  case 0:
-  echo "$x";
-  break;
-
-  case 1:
-  echo "$b = $X";
-  continue;
-}
+?>
